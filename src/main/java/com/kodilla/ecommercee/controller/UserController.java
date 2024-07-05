bcab73b38722525fe57747ef6145fe1bbd818b01
serak <serakrzysztof@gmail.com>
2024-07-05 18:30:34 +0200

    JDP240701-5 ver.3

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.kodilla.ecommercee.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.kodilla.ecommercee.domain.CreateUserDto;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.Random;
 
 @RestController
@@ -14,9 +14,9 @@ public class UserController {
     private final Random random = new Random();
 
     @PostMapping
-    public ResponseEntity<String> createUser() {
-
-        return ResponseEntity.ok("createdUser");
+    public ResponseEntity<String> createUser(@RequestBody CreateUserDto createUserDTO) {
+        String message = "User " + createUserDTO.getUsername() + " created";
+        return ResponseEntity.ok(message);
     }
 
     @PostMapping("/{id}/block")
@@ -25,18 +25,18 @@ public class UserController {
     }
 
     @PostMapping("/generateKey")
-    public ResponseEntity<String> generateRandomKey(@RequestParam String username, @RequestParam String password) {
+    public ResponseEntity<String> generateRandomKey(@PathVariable Long userId) {
         String key = generateRandomString();
         return ResponseEntity.ok(key);
-        }
+    }
 
-    private String generateRandomString() {
-        String characters = "ABCDEFGHIJ0123456789";
-        StringBuilder sb = new StringBuilder(24);
-        for (int i = 0; i < 24; i++) {
-            sb.append(characters.charAt(random.nextInt(characters.length())));
-        }
-        return sb.toString();
+        private String generateRandomString() {
+            String characters = "ABCD0123456789";
+            StringBuilder sb = new StringBuilder(24);
+            for (int i = 0; i < 24; i++) {
+                sb.append(characters.charAt(random.nextInt(characters.length())));
+            }
+            return sb.toString();
     }
 }
 
