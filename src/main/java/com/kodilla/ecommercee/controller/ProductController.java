e35e1d56bc2be057cb995587cb9806b72dda8591
likeahim <michal.panko21@gmail.com>
2024-07-05 17:57:32 +0200

    Task JDP240701-1 ProductController initial implementation

---
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.kodilla.ecommercee.controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/ecommercee/products")
+public class ProductController {
+    @GetMapping
+    public List<Object> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping
+    public Object getProduct(Long productId) {
+        return new Object();
+    }
+
+    @PostMapping
+    public void createProduct(Object productDto) {
+    }
+
+    @PutMapping
+    public Object updateProduct(Object productDto) {
+        return new Object();
+    }
+
+    @DeleteMapping
+    public void deleteProduct(Long productId) {
+    }
+}
