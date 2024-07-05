a65f25ebfe2abde45d4f828557952385febd0392
serak <serakrzysztof@gmail.com>
2024-07-05 17:57:40 +0200

    JDP240701-5 ver.2

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,41 +1,43 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.User;
-import com.kodilla.ecommercee.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.Random;
 
 @RestController
 @RequestMapping("v1/shop/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final Random random = new Random();
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User createdUser = userService.createUser(user);
-        return ResponseEntity.ok(createdUser);
+    public ResponseEntity<String> createUser() {
+
+        return ResponseEntity.ok("createdUser");
     }
 
     @PostMapping("/{id}/block")
-    public ResponseEntity<User> blockUser(@PathVariable Long id) {
-        Optional<User> userOpt = userService.blockUser(id);
-        return userOpt.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<String> blockUser(@PathVariable Long id) {
+        return ResponseEntity.ok("User" + id + "blocked");
     }
 
     @PostMapping("/generateKey")
     public ResponseEntity<String> generateRandomKey(@RequestParam String username, @RequestParam String password) {
-        String key = userService.generateRandomKey(username, password);
-        if (key != null) {
-            return ResponseEntity.ok(key);
-        } else {
-            return ResponseEntity.status(401).body("Invalid credentials");
+        String key = generateRandomString();
+        return ResponseEntity.ok(key);
         }
-    }
 
+    private String generateRandomString() {
+        String characters = "ABCDEFGHIJ0123456789";
+        StringBuilder sb = new StringBuilder(24);
+        for (int i = 0; i < 24; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return sb.toString();
+    }
 }
+
+
