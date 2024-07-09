3680968db92fb1c3c89fab1e0add6be6c2c4ca6b
serak <serakrzysztof@gmail.com>
2024-07-10 00:48:05 +0200

    JDP240701-13 ver.1

---
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "groups")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Product> products = new ArrayList<>();
+}
