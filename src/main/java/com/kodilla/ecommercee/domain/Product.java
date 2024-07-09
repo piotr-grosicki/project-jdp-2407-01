397df1b62f4a4ee95321e353e742cfec15a5aab1
likeahim <michal.panko21@gmail.com>
2024-07-09 14:30:47 +0200

    JDP240701-12 entity Product

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,41 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "product_in_cart",
+            joinColumns = {@JoinColumn(name = "product_id", referencedColumnName = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "cart_id", referencedColumnName = "cart_id")}
+    )
+    private List<Cart> carts;
+}
