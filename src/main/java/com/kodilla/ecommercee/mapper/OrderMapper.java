e1446e6822662c906178929b09228eaec38d8467
jaroo88 <jaroslaw.kolacz@op.pl>
2024-07-21 04:53:16 +0200

    testy group

--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -12,9 +12,9 @@ import java.time.LocalDateTime;
 public class OrderMapper {
     public Order mapToOrder(OrderDto orderDto) {
         return new Order(
-                LocalDateTime.now(), // Using the current date and time for the new order
-                getDefaultUser(),    // Default User object
-                getDefaultCart(),    // Default Cart object
+                LocalDateTime.now(),
+                getDefaultUser(),
+                getDefaultCart(),
                 orderDto.value()
         );
     }
@@ -29,19 +29,17 @@ public class OrderMapper {
 
 
     private User getDefaultUser() {
-        // Here you would typically fetch a default or placeholder user from a service or repository
-        // For demonstration purposes, we return a new User instance with an ID
+
         User defaultUser = new User();
-        defaultUser.setId(1L); // Set default ID or any other required fields
+        defaultUser.setId(1L);
         return defaultUser;
     }
 
 
     private Cart getDefaultCart() {
-        // Here you would typically fetch a default or placeholder cart from a service or repository
-        // For demonstration purposes, we return a new Cart instance with an ID
+
         Cart defaultCart = new Cart();
-        defaultCart.setId(1L); // Set default ID or any other required fields
+        defaultCart.setId(1L);
         return defaultCart;
     }
 }
